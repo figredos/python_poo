d532fa20a0b627586974f9a54f081959e9de9e42
figredos <matheusmfellet@gmail.com>
2023-06-09 00:28:39 -0300

    Atributos de instâncias e o uso de funções __dict__ e vars().

---
+++ introducao/atributos_de_instancia.py
@@ -0,0 +1,14 @@
+# __dict__ e vars para atributos de instância
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+dados = {'nome': 'Giulia', 'idade': 35}
+p1 = Pessoa(**dados)
